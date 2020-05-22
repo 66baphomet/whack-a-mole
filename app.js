7280a39465f3dab9959cfdae7dc46b455faba047
66baphomet <64501452+66baphomet@users.noreply.github.com>
2020-05-22 19:01:04 +0600

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function randomSquare()
     setTimeout( ()=>{
         randomPosition.classList.remove("mole");
         hitPosition=undefined;
-    },700);
+    },1200);
 
     // console.log(hitPosition);
 }
@@ -64,7 +64,7 @@ function moveMole()
 {
     if(startOnce)
     {
-    moleMoveId = setInterval(randomSquare, 1000);
+    moleMoveId = setInterval(randomSquare, 1500);
     timerId = setInterval(countDown, 1000);
     }
 
